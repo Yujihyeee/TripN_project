4e0295aa71c49bd1b4bfe8679d8b3078767ca1c7
Yujihyeee <rladnddl@naver.com>
2021-11-17 17:20:34 +0900

    17

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-
-FROM python:3.8-slim-buster AS builder
-ADD . .
-COPY requirements.txt .
-RUN pip install -r requirements.txt
-CMD ["python","-m","unittest","discover","-s","tests"]
---
+++ project.py
@@ -0,0 +1,32 @@
+class CensusProject:
+    def __init__(self):
+        pass
+
+    def execute(self):
+        pass
+
+
+class Currency:
+    def __init__(self):
+        pass
+
+    def execute(self):
+        pass
+
+
+class Equation:
+    def __init__(self):
+        pass
+
+    def execute(self):
+        pass
+
+
+if __name__ == '__main__':
+    p = CensusProject()
+    p.execute()
+    c = Currency()
+    c.execute()
+    e = Equation()
+    e.execute()
+
