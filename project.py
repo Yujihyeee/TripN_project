22db32155616787a787a6f357e550b101d9af94b
Yujihyeee <rladnddl@naver.com>
2021-11-18 20:24:35 +0900

    20

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-def CensusProject():
+def census_project():
     birth = 86400//17
     death = 86400//12
     arr = 86400//87
@@ -8,20 +8,26 @@ def CensusProject():
     r = int(year) * (birth + arr - death - det) + a
     print(f'{year}년 후의 인구는 {r} 입니다.')
 
-def Currency():
-    money = int(input('금액을 입력하세요'))
-    count = 0
-    money_type = [10000,5000,1000,500,100,50,10,1]
-    for i in money_type:
-        count += money // i
-        money = money % i
-    print(f'{money} 은 만원 {money//10000}장, 오천원 {money//5000}장, 천원 {money//1000}장, 오박원 {money//500}개, 백원 {money//100}개, 십원 {money//1}개 입니다.')
 
-def Equation():
+def currency():
+    money = int(input('금액을 입력하세요'))
+    money_type = [10000, 5000, 1000, 500, 100, 50, 10, 1]
+
+
+    print(f'{n} 은 만원 {m}장, '
+          f'오천원 {o}장, '
+          f'천원 {c}장, '
+          f'오백원 {ob}개, '
+          f'백원 {b}개, '
+          f'십원 {s}개,'
+          f'일원 {i}개 입니다.')
+
+
+def equation():
     pass
 
 
 if __name__ == '__main__':
-    # CensusProject()
-    Currency()
-    # Equation()
+    # census_project()
+    currency()
+    # equation()
