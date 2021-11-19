6a440e80bd876ea202629e3a601d9cf1285b37b9
Yujihyeee <rladnddl@naver.com>
2021-11-19 12:08:29 +0900

    12

--- project.py
+++ project.py
@@ -1,20 +1,25 @@
 def census_project():
-    birth = 86400//17
-    death = 86400//12
-    arr = 86400//87
-    det = 86400//231
-    year = input(f'년 수를 입력하세요')
+    birth = 86400 // 17
+    death = 86400 // 12
+    arr = 86400 // 87
+    det = 86400 // 231
+    print(birth , arr , death , det)
+    year = int(input(f'년 수를 입력하세요'))
     a = 51761891
-    r = int(year) * (birth + arr - death - det) + a
+    r = a + (birth + arr - death - det) * year
     print(f'{year}년 후의 인구는 {r} 입니다.')
 
 
 def currency():
     money = int(input('금액을 입력하세요'))
-    money_type = [10000, 5000, 1000, 500, 100, 50, 10, 1]
-
-
-    print(f'{n} 은 만원 {m}장, '
+    m = money // 10000
+    o = (money % 10000) // 5000
+    c = (money % 5000) // 1000
+    ob = (money % 1000) // 500
+    b = (money % 500) // 100
+    s = (money % 100) // 10
+    i = (money % 10) // 1
+    print(f'입력하신 금액 {money}원은 만원 {m}장, '
           f'오천원 {o}장, '
           f'천원 {c}장, '
           f'오백원 {ob}개, '
@@ -24,10 +29,22 @@ def currency():
 
 
 def equation():
-    pass
+    while True:
+        a = float(input('a의 값을 입력하세요:'))
+        b = float(input('b의 값을 입력하세요:'))
+        c = float(input('c의 값을 입력하세요:'))
+        D = b * b - 4 * a * c
+        if D > 0:
+            r = (-b + (b ** 2 - 4 * a * c) ** 0.5) / (2 * a)
+            r2 = (-b - (b ** 2 - 4 * a * c) ** 0.5) / (2 * a)
+            print(f'실수 해는 {r} 와/과 {r2} 입니다.')
+        elif D == 0:
+            x = -b / (2 * a)
+            print(f'실수 해는 {x} 입니다.')
+        elif D < 0:
+            print("이 방정식의 실수 해는 존재하지 않습니다.")
 
 
 if __name__ == '__main__':
-    # census_project()
-    currency()
-    # equation()
+    equation()
+
